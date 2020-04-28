a84bd7bbccabe6ccff32ee7f2c9ddbd4739e980e
eNobreg <ezra.nobrega@outlook.com>
2020-04-27 23:56:44 -0400

    [New] Initial push

---
+++ api/v1/views/states.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, abort
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states/", strict_slashes=False, Methods=['GET'])
+@app_views.route("/states/<state_id>", strict_slashes=False, Methods='GET')
+def get_states(state_id=None):
+    """
+    Returns state objects based on path
+
+    with state_id: Returns a single state object
+    without state_id: Returns every state
+    """
+    new_list = []
+    key = "State." + state_id
+    if state_id is None:
+        objs = storage.all(State)
+        for key, value in objs.items():
+            new_list.append(value.to_dict())
+    elif key in storage.all(State).keys():
+        new_list.append(storage.all(State)[key].to_dict())
+    else:
+        abort(404)
+    return jsonify(new_list)
