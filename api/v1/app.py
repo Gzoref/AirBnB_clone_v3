1e3ab2abd06481a51c4e3653732c66d4004e95e1
Gzoref <gzoref@gmail.com>
2020-04-27 22:05:42 -0400

    404 handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -8,6 +8,14 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    '''
+    404  route
+    '''
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
