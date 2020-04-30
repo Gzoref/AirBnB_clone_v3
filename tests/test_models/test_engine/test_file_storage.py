565fc6cc4140161737d142589f30ba29e1edb3af
Gzoref <gzoref@gmail.com>
2020-04-29 22:43:47 -0400

    Unit Tests for file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,7 @@ Contains the TestFileStorageDocs classes
 from datetime import datetime
 import inspect
 import models
+from models import storage
 from models.engine import file_storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -113,3 +114,15 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """"Tests if method retrieves one object"""
+        storage = FileStorage()
+        self.assertIs(storage.get(User, "Geoff"), None)
+        
+    unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """""Test if count method counts number of objects in storage"""
+        counter = storage.count()
+        self.assertEqual(models.storage.count('State'), 0)
