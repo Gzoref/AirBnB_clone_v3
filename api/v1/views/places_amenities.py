d270947318f2d6740ac5a283754cd8a49828646f
eNobreg <ezra.nobrega@outlook.com>
2020-04-30 19:40:06 -0400

    [Update] Fixed reversed variables

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -61,10 +61,10 @@ def post_place_amenity(place_id=None, amenity_id=None):
     """
     Post a amenity
     """
-    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    amenity = storage.get(Amenity, amenity_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
