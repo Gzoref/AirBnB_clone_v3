c4c9b41c13168a0af973b758eecd7bebbb00d046
eNobreg <ezra.nobrega@outlook.com>
2020-04-30 19:34:48 -0400

    [Update] Added line for actual deleting, update post route

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -38,9 +38,9 @@ def delete_place_amenity(place_id, amenity_id):
     Deletes a amenity from the database
     """
     place = storage.get(Place, place_id)
-    amenity = storage.get(Amenity, amenity_id)
     if place is None:
         abort(404)
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
@@ -50,6 +50,7 @@ def delete_place_amenity(place_id, amenity_id):
     else:
         if amenity.id not in place.amenity_ids:
             abort(404)
+        place.amenity_ids.remove(amenity.id)
     place.save()
     return jsonify({}), 200
 
@@ -60,17 +61,19 @@ def post_place_amenity(place_id=None, amenity_id=None):
     """
     Post a amenity
     """
-    amenity = storage.get(Amenity, amenity_id)
     place = storage.get(Place, place_id)
     if amenity is None:
         abort(404)
+    amenity = storage.get(Amenity, amenity_id)
     if place is None:
         abort(404)
-    if amenity in place.amenities:
-        return jsonify(amenity.to_dict()), 200
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        if amenity in place.amenities:
+            return jsonify(amenity.to_dict()), 200
         place.amenities.append(amenity)
     else:
-        place.amenity_ids.append('Amenity.' + amenity_id)
+        if amenity_id in place.amenity_ids:
+            return jsonify(amenity.to_dict()), 200
+        place.amenity_ids.append(amenity_id)
     place.save()
     return jsonify(amenity.to_dict()), 201
