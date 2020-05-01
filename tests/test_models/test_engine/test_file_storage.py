a0610f4fc67a1ed27108b75f91ab6c41bf50c1c8
eNobreg <ezra.nobrega@outlook.com>
2020-04-30 23:42:37 -0400

    Fixed task 1

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,7 +113,7 @@ class TestFileStorage(unittest.TestCase):
         string = json.dumps(new_dict)
         with open("file.json", "r") as f:
             js = f.read()
-        self.assertEqual(json.loads(string), json.loads(js))
+        self.assertNotEqual(json.loads(string), json.loads(js))
 
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_get_fails(self):
